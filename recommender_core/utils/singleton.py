a72e3911ff22129168e5d888fad192a0f4e54a22
abdulrhman brighsh <abd.br.1987@gmail.com>
2025-02-23 20:43:44 +0100

    improve singleton

--- recommender_core/utils/singleton.py
+++ recommender_core/utils/singleton.py
@@ -3,5 +3,8 @@ class Singleton(object):
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
+        elif cls.__init__:
+            # call __init__ function just the first time
+            cls.__init__ = lambda self, *args, **kwargs: None   # __init__ function is "<lambda>" now.
         return cls._instance
